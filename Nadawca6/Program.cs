41b747787438e68386aa8ebb1a2e07d1f25d8eb6
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-17 23:25:02 +0200

    Add files via upload

---
+++ Nadawca6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Nadawca6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wyslane");
+            Console.WriteLine("Nacisnij przycisk");
+            Console.ReadKey();
+
+            string queueName = "queueZadanie6";
+
+            var factory = new ConnectionFactory();
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+
+            channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+            var channelProperties = channel.CreateBasicProperties();
+            channelProperties.Headers = new Dictionary<string, object>();
+            {
+                channelProperties.Headers.Add("queueName", queueName);
+                channelProperties.Headers.Add("Autor", "Bartosz Kolakowski");
+            }
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var responseMessage = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Otrzymano odpowiedz: '{responseMessage}'");
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string message = $"Wiadomosc {i} od wysylajacego";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                                    routingKey: queueName,
+                                    basicProperties: channelProperties,
+                                    body: body);
+                Console.WriteLine($"Otrzymano: '{message}'");
+            }
+
+            Console.WriteLine("Nacisnij przycisk");
+            Console.ReadKey();
+
+            channel.Close();
+            conn.Close();
+        }
+    }
+}
---
+++ Odbiorca61/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace Odbiorca1
+{
+    class Program
+    {
+        static readonly object lockObject = new object();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odbiorca 1");
+            Console.WriteLine("Nacisnij przycisk zeby zaczac");
+            Console.ReadKey();
+
+            string queueName = "queueZadanie6";
+
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+                VirtualHost = "/"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    lock (lockObject)
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"Received: '{message}' from {queueName}");
+                        Thread.Sleep(2000);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    }
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine("Nacisnij przycisk zeby skonczyc");
+                Console.ReadKey();
+            }
+        }
+    }
+}
